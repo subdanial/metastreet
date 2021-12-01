c1736de52be4c7312a2d4030dcdc3b1a696ea73a
subdanial <subdanial@gmail.com>
2021-12-01 20:04:37 +0330

    update

--- resources/views/nft.blade.php
+++ resources/views/nft.blade.php
@@ -11,7 +11,7 @@
                 <div class="relative flex items-center justify-center w-full h-full pointer-events-none">
                     <img src="{{ URL::asset('/img/soon.jpg') }}"
                         class="w-full lg:absolute top-0 left-0 pointer-events-none" alt="">
-                    <h1 class="absolute top-64  bottom-0 text-white text-5xl">Coming Soon</h1>
+                    <h1 class="absolute text-white text-5xl">Coming Soon</h1>
                 </div>
 
             </div>
